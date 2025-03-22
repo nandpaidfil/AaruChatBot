3083c30c276ab14f4597baba0c3f13e34672c3b4
NAND7095 <duhgfhfdghg@gmail.com>
2025-03-22 14:32:39 +0530

    Update repo.py

--- ChatBot/modules/repo.py
+++ ChatBot/modules/repo.py
@@ -5,14 +5,14 @@ from ChatBot import app
 @app.on_message(filters.command("repo"))
 async def start(_, msg):
     await msg.reply_photo(
-        photo="https://i.postimg.cc/FF2Jv8D0/ec107964b90c959da231293998b6d73e.jpg",
-        caption="""Hey there, I'm Aaru, your AI chatbot. ♥︎
+        photo="https://files.catbox.moe/extt9m.jpg",
+        caption="""Hey there, I'm Maahira ♥︎
 
 If you want my bot repo, click below to get the source code.
 
-Powered by @CodeSearchDev ✨""",
+Powered by @TMZEROO ✨""",
         reply_markup=InlineKeyboardMarkup([
-            [InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/AsuraaSupports"),
-             InlineKeyboardButton("ʀᴇᴘᴏ", url="https://github.com/CodeSearchDev/AaruChatBot")]
+            [InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/NoxxNetwork"),
+             InlineKeyboardButton("ʀᴇᴘᴏ", url="https://github.com/powerfulboys123/powerfulboys123/blob/main/README.md")]
         ])
     )
